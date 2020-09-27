c09f57a777e0f3b8e550a84e66f7ce87f8efaf27
zuev56 <veranda56@rambler.ru>
2020-09-27 21:37:39 +0300

    Delete old test projects

--- Services/Zs.Service.Home/MyTestWebInterface/Models/TelegramBotApiViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyTestWebInterface.Models
-{
-    public class TelegramBotApiViewModel
-    {
-        public string BotToken { get; set; }
-        public string MethodName { get; set; }
-        public string Response { get; set; }
-    }
-}
